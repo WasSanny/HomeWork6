9f5273e9052bf35b4e7327dacdd377ce7ec1407e
Dmitry Vasiliuk <usy222@mail.ru>
2023-03-06 21:14:08 +0300

    водим м чисел с клавиатуры и считаем количество положительных

---
+++ Task1/Program.cs
@@ -0,0 +1,47 @@
+﻿//Пользователь вводит с клавиатуры М чисел. Написать программу, которая
+// считает сколько из них больше 0.
+
+Task_1();
+
+void Task_1()
+{
+  int sum = 0;
+
+  for (int i = 0; i < 5; i++)
+  {
+    int a = ReadInt("Введите число");
+
+    if (a > 0)
+    {
+    GetSumPositiveNumbers(a);
+    sum++;
+    }
+  }
+  Console.WriteLine($"Количество введенных положительных чисел равно: {sum}");
+}
+
+int ReadInt(string argument)        
+{                                    
+  Console.Write($"{argument}: ");
+  int number;
+
+  while (!int.TryParse(Console.ReadLine(), out number)) 
+  {
+      Console.WriteLine("Error! Try again.");          
+  }
+
+  return number;                                        
+}
+
+void GetSumPositiveNumbers(int number)        
+{ 
+    if (number > 0)
+    { 
+      Console.WriteLine($" {number} - Положитльное число!");
+    }
+    else
+    {
+      Console.WriteLine($" {number} - Отрицательное число!");
+    }
+    
+}
